57585768577790ea5db548d6b47a18fe7730c8da
Leamsi Escribano <leamsii.e@gmail.com>
2020-06-03 13:30:30 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+  attr_accessor :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, age)
+    if @roster.key?(age)
+      @roster[age] << name
+    else
+      @roster[age] = [name]
+    end
+  end
+  
+  def grade(grade)
+    @roster.select{|g| g == grade}
+  end
+  
+  def sort
+    @roster.sort_by { |w| w[0].match?(/[A-Z]/) ? 0 : 1 }
+  end
+end
