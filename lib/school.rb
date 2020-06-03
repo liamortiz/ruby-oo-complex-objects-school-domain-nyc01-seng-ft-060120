8c20b051817fe3478eff08fe08ff427a106e9fdf
Leamsi Escribano <leamsii.e@gmail.com>
2020-06-03 13:37:49 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+  attr_accessor :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, age)
+    if @roster.key?(age)
+      @roster[age] << name
+    else
+      @roster[age] = [name]
+    end
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    sorted_hash = {}
+    @roster.each do |key, value|
+      sorted_hash[key] = value.sort
+    end
+    sorted_hash
+  end
+end
